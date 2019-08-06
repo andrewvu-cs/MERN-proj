a012f2f684bf4a907f3e6eb9da77dd926cc09c2c
avu916 <andrewvu.cs@gmail.com>
2019-08-05 17:02:33 -0700

    updated server.js, able to connect to mongoDB

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// configures so we can have variable in our env variable
+require('dotenv').config();
+
+// Creates our express server on our port
+const app = express();
+const port = process.env.PORT || 5000;
+
+// CORS middleware 
+app.use(cors());
+
+// Parses json sending and receiving
+app.use(express.json());
+
+// Database uri recieved from mongodb atalas
+const uri = process.env.ATLAS_URI;
+
+// how to start connection
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+// Starts up the server, listening on a certain port
+app.listen(port, ()=> {
+    console.log(`Server is running on port: ${port}`);
+});
