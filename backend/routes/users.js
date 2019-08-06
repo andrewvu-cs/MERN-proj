bb8d38c61c68b678736282a33168b96869598306
avu916 <andrewvu.cs@gmail.com>
2019-08-05 18:38:16 -0700

    updated routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,21 +1,27 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+
+// endpoint that handle incoming http get requests
 router.route('/').get((req, res) => {
+    // Mongoose method that retrieves data of all users, returns a promise
     User.find()
         .then(users=>res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+
+// endpoint that handle http post requests
 router.route('/add').post((req, res) => {
     const username = req.body.username;
 
+    // create new instance of user
     const newUser = new User({username});
 
+    // saves it to mongodb atlas database
     newUser.save()
         .then(() => res.json('User added!'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
-
 
 module.exports = router;
